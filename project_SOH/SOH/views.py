486795ba7316b458eff4549d3fa1835c3fdd774f
dev-hjJoo <20163030@changwon.ac.kr>
2020-11-11 00:47:24 +0900

    기본 페이지 띄우기 + gitignore 처리

--- project_SOH/SOH/views.py
+++ project_SOH/SOH/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    return render(request, 'SOH/index.html')
---
+++ project_SOH/project_SOH/my_settings.py
@@ -0,0 +1,11 @@
+# 공유하면 안 되는 정보들 (.gitignore 처리)
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'skein_of_heart',
+        'USER': 'developer',
+        'PASSWORD': 'skeinofheart97!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
